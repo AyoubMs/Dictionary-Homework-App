0f0aaf7a4b812981d753d0502438e1a102cad4e2
AyoubMS <chahayb563@gmail.com>
2023-07-05 12:44:05 +0200

    add app

---
+++ DictionaryHomework/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+string? idText;
+
+do
+{
+
+    Dictionary<int, string> employees = new Dictionary<int, string>();
+
+    employees.Add(1, "Ayoub");
+    employees.Add(2, "Fahd");
+    employees.Add(3, "Chahido");
+
+    Console.Write("Enter a valid id (or exit to exit the app): ");
+
+    idText = Console.ReadLine();
+
+    bool isValid = int.TryParse(idText, out int id);
+
+    if (!isValid || !employees.Keys.Contains(id))
+    {
+        Console.WriteLine("Please enter a valid number");
+    }
+
+    if (isValid && employees.Keys.Contains(id))
+    {
+        Console.WriteLine($"The employee with id {id} is {employees[id]}");
+    }
+} while (idText != "exit");
